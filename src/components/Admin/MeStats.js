38b4f32b75fe892de7efe79d36bd7202e0ccc1e7
Marcus <mojave27@gmail.com>
2020-01-12 22:14:18 -0700

    add range sliders

--- src/components/Admin/MeStats.js
+++ src/components/Admin/MeStats.js
@@ -1,44 +1,101 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
+import { useState } from 'react'
 import Select from '../inputs/Select'
-import {
-  meStatsContainer,
-  meEnergy,
-  meMood,
-  meSleep,
-  meWeight
-} from '../../styles/WoDayStyles'
+import RangeSlider from '../inputs/RangeSlider'
+import { } from '../../styles/WoDayStyles'
 
 const MeStats = props => {
+  let [energyRange, setEnergyRange] = useState(50)
+  let [sleepRange, setSleepRange] = useState(50)
+
+  const handleSliderChange = event => {
+    let id = event.target.id
+    let value = event.target.value
+    switch (id) {
+      case 'energyRange':
+        console.log('energyRange update.')
+        setEnergyRange(value)
+        break
+      case 'sleepRange':
+        setSleepRange(value)
+        break
+      case 'moodRange':
+        break
+      default:
+        console.log('Sorry, no match for ' + id)
+    }
+    // console.log(`id: ${event.target.id}`)
+    // console.log(`id: ${event.target.value}`)
+  }
+
   return (
-    // <div css={meStatsContainer}>
     <div>
-      <div css={meWeight}>
+      <div >
         <div
           style={{
             display: 'flex',
             flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <label style={{ fontWeight: '700', width:'12%' }}>Weight</label>
-          <Select jssClass={{float:'left'}} options={['1', '2', '3', '4']} />
-          <div css={meEnergy} style={{width:'70%'}}>energy</div>
-        </div>
-      </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            border: '1px solid green',
-            justifyContent: 'center',
+            justifyContent: 'space-between',
             alignItems: 'center'
           }}
         >
-      <div css={meSleep} style={{width:'50%'}}>sleep</div>
-      <div css={meMood} style={{width:'50%'}}>mood</div>
-    </div>
+          <label style={{ fontWeight: '700', float:'left',width: '12%' }}>Weight</label>
+          <Select jssClass={{ float: 'left' }} options={[...Array(350).keys()]} />
+          <div  style={{ width: '30%' }}>
+            <label
+              style={{
+                float: 'left',
+                display: 'inline-block',
+                fontWeight: '700',
+                width: '20%'
+              }}
+            >
+              Energy
+            </label>
+            <RangeSlider
+              jssClass={{float: 'left'}}
+              id='energyRange'
+              value={energyRange}
+              onChange={handleSliderChange}
+            />
+          </div>
+          <div style={{ width: '30%' }}>
+            <label
+              style={{
+                float: 'left',
+                display: 'inline-block',
+                fontWeight: '700',
+                width: '20%'
+              }}
+            >
+              Sleep
+            </label>
+            <RangeSlider
+              jssClass={{float: 'left'}}
+              id='sleepRange'
+              value={sleepRange}
+              onChange={handleSliderChange}
+            />
+          </div>
+        </div>
+      </div>
+      {/* <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          border: '1px solid green',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <div css={meSleep} style={{ width: '50%' }}>
+          sleep
+        </div>
+        <div css={meMood} style={{ width: '50%' }}>
+          mood
+        </div>
+      </div> */}
     </div>
   )
 }
--- src/styles/WoDayStyles.js
+++ src/styles/WoDayStyles.js
@@ -40,6 +40,46 @@ export const meMood = css({
   border:'1px solid cyan'
 })
 
+
+// range slider styles ---------------------------------- //
+// export let slidecontainer = {
+//   width: '100%' /* Width of the outside container */
+// }
+
+/* The slider itself */
+export let slider = {
+  webkitAppearance: 'none',  /* Override default CSS styles */
+  appearance: 'none',
+  width: '200px', /* Full-width */
+  height: '15px', /* Specified height */
+  background: activeTheme.color4.hex, /* Grey background */
+  outline: 'none', /* Remove outline */
+  opacity: '0.7', /* Set transparency (for mouse-over effects on hover) */
+  webkitTransition: '.2s', /* 0.2 seconds transition on hover */
+  transition: 'opacity .2s',
+  '&:hover': {
+    opacity: '1'
+  },
+  '&::-webkit-slider-thumb': {
+  webkitAppearance: 'none', /* Override default look */
+  appearance: 'none',
+  width: '25px', /* Set a specific slider handle width */
+  height: '25px', /* Slider handle height */
+  background: activeTheme.color2.hex, /* Green background */
+  border: activeTheme.color3.hex,
+  borderRadius:'15px',
+  cursor: 'pointer' /* Cursor on hover */
+  },
+  '&::-moz-range-thumb': {
+  width: '25px', /* Set a specific slider handle width */
+  height: '25px', /* Slider handle height */
+  background: activeTheme.color2.hex, /* Green background */
+  cursor: 'pointer' /* Cursor on hover */
+  }
+}
+
+
+
 // cardio styles ---------------------------------------- //
 
 
