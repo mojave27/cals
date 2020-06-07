532705e48ab2351d567fcb54422a5862302e8643
Marcus <mojave27@gmail.com>
2020-06-07 14:46:23 -0700

    Update axiosConfig.js
    removed unused stuff.

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,10 +1,5 @@
 export const config = {
     // baseURL: 'http://localhost:3030'
-    baseURL: 'http://ec2-100-26-232-196.compute-1.amazonaws.com:3030'
+    // this value needs to change on each restart of the ec2 instance.
+    baseURL: 'http://ec2-54-146-69-191.compute-1.amazonaws.com:3030'
 }
-
-// export const axios = axios.create({
-//   baseURL: "http://localhost:3030/",
-//   timeout: 10000
-//   headers: {'X-Custom-Header': 'foobar'}
-// });
