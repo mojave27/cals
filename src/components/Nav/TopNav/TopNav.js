e63fd31495097c69eb1bf34cf09f49f7286a9404
Marcus <mojave27@gmail.com>
2020-06-21 17:32:10 -0700

    updated thmeing

--- src/components/Nav/TopNav/TopNav.js
+++ src/components/Nav/TopNav/TopNav.js
@@ -1,99 +1,153 @@
-import { Component } from "react";
+import React, { useContext, useState } from 'react'
 /** @jsx jsx */
 import { jsx } from '@emotion/core'
-import { Link } from "@reach/router";
-import { menuConfig } from './topNavMenuConfig';
-import { navbar, dropdown, dropbtn, dropdownContent } from '../../../styles/topNavStyles'
+import { Link } from '@reach/router'
+import { menuConfig } from './topNavMenuConfig'
+import ThemeContext from '../../../context/ThemeContext'
 
-// import "./TopNav.css";
+const TopNav = props => {
+  let [display, setDisplay] = useState({})
+  let context = useContext(ThemeContext)
 
-class TopNav extends Component {
-    state = {
-        display: {}
-    };
-
-    handleClick = event => {
-        this.toggleDisplay(event, "none");
-    };
-
-    mouseOver = event => {
-        this.toggleDisplay(event, "block");
-    };
-
-    mouseOut = event => {
-        this.toggleDisplay(event, "none");
-    };
-
-    toggleDisplay = (event, displayType) => {
-        const menuName = event.target.getAttribute("menu-name");
-        let display = this.state.display;
-        display[menuName] = displayType;
-        this.setState({ display: display });
-    };
-
-    renderMenu = (menuConfig, index) => {
-        if (menuConfig.type === 'button') {
-            return this.renderButton(menuConfig, index)
-        }
-        if (menuConfig.type === 'dropdown') {
-            return this.renderDropDownMenu(menuConfig, index)
-        }
+  let styles = {
+    navbar: {
+      overflow: 'hidden',
+      backgroundColor: context.theme.color5.hex,
+      borderBottom: `solid 1px ${context.theme.color3.hex}`,
+      fontFamily: 'Arial',
+      fontSize: '14px'
+    },
+    dropdown: {
+      float: 'left',
+      overflow: 'hidden'
+    },
+    dropbtn: {
+      border: 'none',
+      outline: 'none',
+      padding: '14px 16px;',
+      backgroundColor: 'inherit',
+      color: context.theme.color5_text.hex,
+      fontSize: '14px',
+      fontFamily: 'inherit',
+      margin: '0',
+      '&:hover': {
+        backgroundColor: context.theme.color4.hex
+      }
+    },
+    dropdownContent: {
+      display: 'none',
+      position: 'absolute',
+      backgroundColor: context.theme.color2.hex,
+      color: context.theme.color2_text.hex,
+      minWidth: '160px',
+      boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)',
+      zIndex: '1',
+      /* Links inside the dropdown */
+      '& a': {
+        float: 'none',
+        color: context.theme.color5.hex,
+        padding: '12px 16px',
+        textDecoration: 'none',
+        display: 'block',
+        textAlign: 'left'
+      },
+      /* Add background color to dropdown links on hover */
+      '& a:hover': {
+        backgroundColor: context.theme.color4.hex,
+        color: context.theme.color4_text.hex,
+        border: `1px solid ${context.theme.color2.rgba(0.75)}`
+      }
     }
+  }
 
-    renderButton = (menuConfig, index) => {
-        let menuName = menuConfig.name;
-        return (<div key={index} css={dropdown}>
-            <Link to={menuConfig.link.to} >
-            <button
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                menu-name={menuName}
-                css={dropbtn}
-            >
-                {menuName}
-            </button>
-            </Link>
-        </div>)
+  const handleClick = event => {
+    toggleDisplay(event, 'none')
+  }
+
+  const mouseOver = event => {
+    toggleDisplay(event, 'block')
+  }
+
+  const mouseOut = event => {
+    toggleDisplay(event, 'none')
+  }
+
+  const toggleDisplay = (event, displayType) => {
+    const menuName = event.target.getAttribute('menu-name')
+    let newDisplay = { ...display }
+    newDisplay[menuName] = displayType
+    setDisplay(newDisplay)
+  }
+
+  const renderMenu = (menuConfig, index) => {
+    if (menuConfig.type === 'button') {
+      return renderButton(menuConfig, index)
     }
-
-    renderDropDownMenu = (menuConfig, index) => {
-        let menuName = menuConfig.name;
-        return (<div key={index} css={dropdown}>
-            <button
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                menu-name={menuName}
-                css={dropbtn}
-            >
-                {menuName}
-                <i className="fa fa-caret-down" />
-            </button>
-            <div
-                style={{ display: this.state.display[menuName] }}
-                menu-name={menuName}
-                onClick={this.handleClick}
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                css={dropdownContent}
-            >
-                {menuConfig.items.map((menuItem, index) => {
-                    return (<Link key={index} menu-name={menuName} to={menuItem.to}>{menuItem.text}</Link>)
-                })}
-            </div>
-        </div>)
-    };
-
-    render() {
-        return (
-            <div>
-                <div css={navbar}>
-                    {menuConfig.map((menu, index) => {
-                        return this.renderMenu(menu, index)
-                    })}
-                </div>
-            </div>
-        );
+    if (menuConfig.type === 'dropdown') {
+      return renderDropDownMenu(menuConfig, index)
     }
+  }
+
+  const renderButton = (menuConfig, index) => {
+    let menuName = menuConfig.name
+    return (
+      <div key={index} css={styles.dropdown}>
+        <Link to={menuConfig.link.to}>
+          <button
+            onMouseOver={mouseOver}
+            onMouseOut={mouseOut}
+            menu-name={menuName}
+            css={styles.dropbtn}
+          >
+            {menuName}
+          </button>
+        </Link>
+      </div>
+    )
+  }
+
+  const renderDropDownMenu = (menuConfig, index) => {
+    let menuName = menuConfig.name
+    return (
+      <div key={index} css={styles.dropdown}>
+        <button
+          onMouseOver={mouseOver}
+          onMouseOut={mouseOut}
+          menu-name={menuName}
+          css={styles.dropbtn}
+        >
+          {menuName}
+          <i className='fa fa-caret-down' />
+        </button>
+        <div
+          style={{ display: display[menuName] }}
+          menu-name={menuName}
+          onClick={handleClick}
+          onMouseOver={mouseOver}
+          onMouseOut={mouseOut}
+          css={styles.dropdownContent}
+        >
+          {menuConfig.items.map((menuItem, index) => {
+            return (
+              <Link key={index} menu-name={menuName} to={menuItem.to}>
+                {menuItem.text}
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div css={styles.navbar}>
+        {menuConfig.map((menu, index) => {
+          return renderMenu(menu, index)
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default TopNav;
+export default TopNav
