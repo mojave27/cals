ee11ffd7bc4f40d103387aa0b8deaaf6244815b4
Marcus <mojave27@gmail.com>
2020-11-28 08:35:11 -0700

    switched to mui topnav.

--- src/components/Nav/TopNav/TopNav.js
+++ src/components/Nav/TopNav/TopNav.js
@@ -1,18 +1,111 @@
-// eslint-disable-next-line no-unused-vars
-import { useContext, useState } from 'react'
-/** @jsx jsx */
-import { jsx } from '@emotion/core'
-import OrigNav from './OrigNav'
+import React, { useContext, useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 
+import { makeStyles } from '@material-ui/core/styles'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ThemeContext from '../../../context/ThemeContext'
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+import Collapse from '@material-ui/core/Collapse';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  toolbar: {
+   backgroundColor: theme.color5.hex,
+   color: theme.color5_text.hex,
+   borderBottom: `1px solid ${theme.color3.hex}`
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  list: {
+    width: 250,
+  }
+}))
 
 const TopNav = props => {
-  // const classes = useStyles()
+  const themeContext = useContext(ThemeContext)
+  const classes = useStyles(themeContext.theme)
+  const [state, setState] = useState(false)
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState(open);
+  }
+
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  )
+
   return (
-      <AppBar position='sticky' elevation={0} style={{marginBottom:'30px'}}>
-        <OrigNav {...props} />
+    <div className={classes.root}>
+      <AppBar position='sticky' elevation={0} style={{ marginBottom: '30px' }}>
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          >
+            <MenuIcon onClick={toggleDrawer(true)} />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            wo-log
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
       </AppBar>
+      <Drawer anchor={'left'} open={state} onClose={toggleDrawer(false)}>
+            {list()}
+          </Drawer>
+    </div>
   )
 }
 
 export default TopNav
+
+
+
+
+
