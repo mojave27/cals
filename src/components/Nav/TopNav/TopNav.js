6724fb75736a8c4556ee95f0ec051fa3e459b4fe
Marcus <mojave27@gmail.com>
2020-11-28 09:05:05 -0700

    toggle drawer

--- src/components/Nav/TopNav/TopNav.js
+++ src/components/Nav/TopNav/TopNav.js
@@ -8,25 +8,29 @@ import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import ThemeContext from '../../../context/ThemeContext'
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
 
-import Collapse from '@material-ui/core/Collapse';
+import Collapse from '@material-ui/core/Collapse'
+import StarBorder from '@material-ui/icons/StarBorder'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
   toolbar: {
-   backgroundColor: theme.color5.hex,
-   color: theme.color5_text.hex,
-   borderBottom: `1px solid ${theme.color3.hex}`
+    backgroundColor: theme.color5.hex,
+    color: theme.color5_text.hex,
+    borderBottom: `1px solid ${theme.color3.hex}`
   },
   menuButton: {
     marginRight: theme.spacing(2)
@@ -35,33 +39,59 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   },
   list: {
-    width: 250,
-  }
+    width: 250
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    // ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
 }))
 
 const TopNav = props => {
   const themeContext = useContext(ThemeContext)
   const classes = useStyles(themeContext.theme)
-  const [state, setState] = useState(false)
+  const [expandSection, setExpandSection] = React.useState(false)
+  const [drawerOpen, setDrawerOpen] = React.useState(false)
 
-  const toggleDrawer = (open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true)
+  }
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false)
+  }
+
+  const handleClick = () => {
+    setExpandSection(!expandSection)
+  }
+
+  const toggleDrawer = open => event => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return
     }
-    setState(open);
+    setDrawerOpen(open)
   }
 
   const list = () => (
     <div
       className={classes.list}
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
+      role='presentation'
+      // onClick={handleDrawerClose()}
+      // onKeyDown={toggleDrawer(false)}
     >
       <List>
         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -70,11 +100,31 @@ const TopNav = props => {
       <List>
         {['All mail', 'Trash', 'Spam'].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
+      <Divider />
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary='Inbox' />
+        {expandSection ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={expandSection} timeout='auto' unmountOnExit>
+        <List component='div' disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary='Starred' />
+          </ListItem>
+        </List>
+      </Collapse>
     </div>
   )
 
@@ -88,7 +138,7 @@ const TopNav = props => {
             color='inherit'
             aria-label='menu'
           >
-            <MenuIcon onClick={toggleDrawer(true)} />
+            <MenuIcon onClick={handleDrawerOpen} />
           </IconButton>
           <Typography variant='h6' className={classes.title}>
             wo-log
@@ -96,16 +146,22 @@ const TopNav = props => {
           <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
-      <Drawer anchor={'left'} open={state} onClose={toggleDrawer(false)}>
-            {list()}
-          </Drawer>
+      <Drawer
+        anchor={'left'}
+        open={drawerOpen}
+        variant={'persistent'}
+        onClose={handleDrawerClose}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        {list()}
+      </Drawer>
     </div>
   )
 }
 
 export default TopNav
-
-
-
-
-
