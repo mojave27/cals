5b0967d25365e3fbc1db71fb276c7f4511ce86ca
Marcus <mojave27@gmail.com>
2021-01-07 18:49:44 -0700

    removed the day array.

--- src/components/WoDay/CalendarView.js
+++ src/components/WoDay/CalendarView.js
@@ -10,16 +10,6 @@ import {
   TableRow
 } from '@material-ui/core'
 
-const day = {
-  0: 'Sunday',
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday'
-}
-
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
