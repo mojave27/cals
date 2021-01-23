698ea6a928b60b849c011bc9de4c2194452d27e0
Marcus <mojave27@gmail.com>
2021-01-23 08:39:09 -0700

    added spinner for addExercise.

--- src/components/exercises/ExerciseForm.js
+++ src/components/exercises/ExerciseForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import BasicSpinner from '../spinners/BasicSpinner'
 import TextInput from '../inputs/TextInput'
 import Select from '../inputs/Select.mui'
 import FormButton from '../inputs/FormButton'
@@ -15,9 +16,11 @@ const options = [
 const ExerciseForm = props => {
   const [exerciseName, setExerciseName] = useState('')
   const [exerciseType, setExerciseType] = useState('')
+  const [showSpinner, setShowSpinner] = useState(false)
   const isMobile = useMediaQuery('(max-width:768px)');
 
-  const addExerciseToDb = () => {
+  const addExerciseToDb = async () => {
+    setShowSpinner(true)
     const exercise = {
       name: exerciseName,
       reps: '',
@@ -25,6 +28,7 @@ const ExerciseForm = props => {
       id: ''
     }
     addExercise(exercise).then(response => {
+      setShowSpinner(false)
       props.done()
     })
   }
@@ -49,6 +53,8 @@ const ExerciseForm = props => {
   }
 
   return (
+    <React.Fragment>
+    <BasicSpinner show={showSpinner} />
     <Box style={{ width: isMobile ? '80%' : '20%', padding:'15px', margin: 'auto', textAlign: 'center' }}>
       <Grid container justify={'center'} alignItems={'center'} spacing={2}>
         <Grid item xs={12} sm={12}>
@@ -72,6 +78,7 @@ const ExerciseForm = props => {
         </Grid>
       </Grid>
     </Box>
+    </React.Fragment>
   )
 }
 
