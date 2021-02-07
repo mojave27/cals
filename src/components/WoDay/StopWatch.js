424e3fdd0b8a9263f251330a71d6b34122d7a02d
Marcus <mojave27@gmail.com>
2021-02-07 10:09:43 -0700

    centered more icon.  switched to moreHoriz from moreVert

--- src/components/WoDay/StopWatch.js
+++ src/components/WoDay/StopWatch.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Button, IconButton, Popover } from '@material-ui/core'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { Button, Grid, IconButton, Popover } from '@material-ui/core'
+// import MoreVertIcon from '@material-ui/icons/MoreVert'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import ThemeContext from 'context/ThemeContext'
 
 class Stopwatch extends Component {
   state = {
@@ -51,11 +53,11 @@ class Stopwatch extends Component {
   }
 
   handleMenuClick = event => {
-    this.setState({anchorEl: event.currentTarget, showMenu: true})
+    this.setState({ anchorEl: event.currentTarget, showMenu: true })
   }
 
   handleMenuClose = () => {
-    this.setState({showMenu: false})
+    this.setState({ showMenu: false })
   }
 
   render() {
@@ -65,75 +67,97 @@ class Stopwatch extends Component {
     let minutes = ('0' + (Math.floor(timerTime / 60000) % 60)).slice(-2)
     let hours = ('0' + Math.floor(timerTime / 3600000)).slice(-2)
     return (
-      <div style={{ fontSize: 'calc(3vw + 30px)', flexGrow: 1 }}>
-        {hours}:{minutes}:{seconds}
-        <span style={{ fontSize: '0.5em' }}>:{centiseconds}</span>
-        {this.state.timerOn === false && this.state.timerTime === 0 && (
-          <Button color={'inherit'} onClick={this.startTimer}>
-            {'Start'}
-          </Button>
-        )}
-        {this.state.timerOn === true && (
-          <Button color={'inherit'} onClick={this.stopTimer}>
-            {'Stop'}
-          </Button>
-        )}
-        <Button color={'inherit'} onClick={this.props.onClose}>
-          {'Hide'}
-        </Button>
-        {this.state.timerOn === false && this.state.timerTime > 0 && (
-          <div style={{ display: 'inline-block' }}>
-            <IconButton color={'inherit'} aria-label='More' onClick={this.handleMenuClick}>
-              <MoreVertIcon />
-            </IconButton>
-            <Popover
-              anchorEl={this.state.anchorEl}
-              open={this.state.showMenu}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              onClose={this.handleMenuClose}
-              disableRestoreFocus
-            >
-              <React.Fragment>
-              {this.state.timerOn === true && (
-                <div>
-                  <Button color={'inherit'} onClick={this.stopTimer}>
-                    {'Stop'}
-                  </Button>
-                </div>
-              )} 
-                {this.state.timerOn === false && this.state.timerTime > 0 && (
-                  <div>
-                    <Button color={'inherit'} onClick={this.startTimer}>
-                      {'Resume'}
-                    </Button>
-                  </div>
-                )}
-                {this.state.timerOn === false && this.state.timerTime > 0 && (
-                  <div>
-                    <Button color={'inherit'} onClick={this.resetTimer}>
-                      {'Reset'}
-                    </Button>
-                  </div>
-                )}
-                {this.state.timerOn === false && this.state.timerTime > 0 && (
-                  <div>
-                    <Button color={'inherit'} onClick={this.saveToDuration}>
-                      {'Save to Duration'}
-                    </Button>
-                  </div>
-                )}
-              </React.Fragment>
-            </Popover>
+      <Grid container direction={'row'} style={{border:'1px solid red'}}i spacing={2}>
+        <Grid item style={{border:'1px solid blue'}}>
+          <div style={{ fontSize: 'calc(3vw + 30px)', flexGrow: 1 }}>
+            {hours}:{minutes}:{seconds}
+            <span style={{ fontSize: '0.5em' }}>:{centiseconds}</span>
           </div>
-        )}
-      </div>
+        </Grid>
+        <Grid item style={{border:'1px solid green'}}>
+          <Grid container direction={'column'} style={{border:'1px solid orange'}}>
+            {this.state.timerOn === false && this.state.timerTime === 0 && (
+              <Grid item>
+                <Button color={'inherit'} onClick={this.startTimer}>
+                  {'Start'}
+                </Button>
+              </Grid>
+            )}
+            {this.state.timerOn === true && (
+              <Grid item>
+                <Button color={'inherit'} onClick={this.stopTimer}>
+                  {'Stop'}
+                </Button>
+              </Grid>
+            )}
+            <Grid item>
+              <Button color={'inherit'} onClick={this.props.onClose}>
+                {'Hide'}
+              </Button>
+            </Grid>
+            {this.state.timerOn === false && this.state.timerTime > 0 && (
+              // <div style={{ display: 'inline-block' }}>
+              <div style={{textAlign: 'center'}}>
+                <IconButton
+                  color={'inherit'}
+                  aria-label='More'
+                  onClick={this.handleMenuClick}
+                  size={'small'}
+                >
+                  {/* <MoreVertIcon /> */}
+                  <MoreHorizIcon />
+                </IconButton>
+                <Popover
+                  anchorEl={this.state.anchorEl}
+                  open={this.state.showMenu}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left'
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left'
+                  }}
+                  onClose={this.handleMenuClose}
+                  disableRestoreFocus
+                >
+                  <React.Fragment>
+                    {this.state.timerOn === true && (
+                      <div>
+                        <Button color={'inherit'} onClick={this.stopTimer}>
+                          {'Stop'}
+                        </Button>
+                      </div>
+                    )}
+                    {this.state.timerOn === false && this.state.timerTime > 0 && (
+                      <div>
+                        <Button color={'inherit'} onClick={this.startTimer}>
+                          {'Resume'}
+                        </Button>
+                      </div>
+                    )}
+                    {this.state.timerOn === false && this.state.timerTime > 0 && (
+                      <div>
+                        <Button color={'inherit'} onClick={this.resetTimer}>
+                          {'Reset'}
+                        </Button>
+                      </div>
+                    )}
+                    {this.state.timerOn === false && this.state.timerTime > 0 && (
+                      <div>
+                        <Button color={'inherit'} onClick={this.saveToDuration}>
+                          {'Save to Duration'}
+                        </Button>
+                      </div>
+                    )}
+                  </React.Fragment>
+                </Popover>
+              </div>
+            )}
+          </Grid>
+          {/* </div> */}
+        </Grid>
+      </Grid>
     )
   }
 }
